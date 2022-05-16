7eaf6bdad9bb12a13e691f1204dccf4a1ba2d57f
Anurag Surapaneni <anurag.surapaneni@bsc.es>
2022-05-16 18:05:37 +0200

    added gpu cuda

---
+++ GPU/gri_cuda/jacobs/jac_include.cuh
@@ -0,0 +1,15 @@
+#ifndef JAC_INCLUDE_H
+#define JAC_INCLUDE_H
+#include "jacob_0.cuh"
+#include "jacob_1.cuh"
+#include "jacob_2.cuh"
+#include "jacob_3.cuh"
+#include "jacob_4.cuh"
+#include "jacob_5.cuh"
+#include "jacob_6.cuh"
+#include "jacob_7.cuh"
+#include "jacob_8.cuh"
+#include "jacob_9.cuh"
+#include "jacob_10.cuh"
+#endif
+
