49bbe02ea4a11f42d6d11ddd20d1aa287415191d
alvaro.moure1998 <alvaro.moure1998@gmail.com>
2022-08-31 11:58:49 +0200

    Mechanisms generator update and eliminate duplicate mechanisms

--- pyJac_mechanism_generator.sh
+++ pyJac_mechanism_generator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pyjac --lang c --input ext_install/cantera/share/cantera/data/gri30.cti -b src/Mechanism/GPU
-pyjac --lang cuda --input ext_install/cantera/share/cantera/data/gri30.cti -b src/Mechanism/GPU
+pyjac --lang c --input resources/mechanisms/gri30.cti -b src/Mechanism/GPU
+pyjac --lang cuda --input resources/mechanisms/gri30.cti -b src/Mechanism/GPU
